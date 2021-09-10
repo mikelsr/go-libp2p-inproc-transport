3ff9c449388e4bd6b03b707d8276931fc8588c18
Louis Thibault <l.thibault@sentimens.com>
2021-09-09 21:15:14 -0400

    Fix libp2p imports

--- go.mod
+++ go.mod
@@ -4,9 +4,9 @@ go 1.16
 
 require (
 	github.com/google/uuid v1.3.0
-	github.com/libp2p/go-libp2p-core v0.10.0
+	github.com/libp2p/go-libp2p v0.15.0
+	github.com/libp2p/go-libp2p-core v0.9.0
 	github.com/lthibault/util v0.0.10
 	github.com/multiformats/go-multiaddr v0.4.0
-	github.com/multiformats/go-multihash v0.0.16 // indirect
 	github.com/stretchr/testify v1.7.0
 )
--- inproc_test.go
+++ inproc_test.go
@@ -1,77 +1,77 @@
 package inproc_test
 
-// import (
-// 	"bytes"
-// 	"context"
-// 	"io"
-// 	"testing"
+import (
+	"bytes"
+	"context"
+	"io"
+	"testing"
 
-// 	libp2p "github.com/libp2p/go-libp2p"
-// 	"github.com/libp2p/go-libp2p-core/host"
-// 	"github.com/libp2p/go-libp2p-core/network"
-// 	inproc "github.com/lthibault/go-libp2p-inproc-transport"
-// 	"github.com/stretchr/testify/require"
-// )
+	libp2p "github.com/libp2p/go-libp2p"
+	"github.com/libp2p/go-libp2p-core/host"
+	"github.com/libp2p/go-libp2p-core/network"
+	inproc "github.com/lthibault/go-libp2p-inproc-transport"
+	"github.com/stretchr/testify/require"
+)
 
-// func TestIntegration(t *testing.T) {
-// 	t.Parallel()
+func TestIntegration(t *testing.T) {
+	t.Parallel()
 
-// 	t.Run("SymmetricListen", func(t *testing.T) {
-// 		t.Parallel()
+	t.Run("SymmetricListen", func(t *testing.T) {
+		t.Parallel()
 
-// 		tpt := inproc.New(inproc.WithEnv(inproc.NewEnv()))
+		tpt := inproc.New(inproc.WithEnv(inproc.NewEnv()))
 
-// 		h0, err := libp2p.New(context.Background(),
-// 			libp2p.Transport(tpt),
-// 			libp2p.ListenAddrStrings("/inproc/h0"))
-// 		require.NoError(t, err)
+		h0, err := libp2p.New(context.Background(),
+			libp2p.Transport(tpt),
+			libp2p.ListenAddrStrings("/inproc/h0"))
+		require.NoError(t, err)
 
-// 		h1, err := libp2p.New(context.Background(),
-// 			libp2p.Transport(tpt),
-// 			libp2p.ListenAddrStrings("/inproc/h1"))
-// 		require.NoError(t, err)
+		h1, err := libp2p.New(context.Background(),
+			libp2p.Transport(tpt),
+			libp2p.ListenAddrStrings("/inproc/h1"))
+		require.NoError(t, err)
 
-// 		testFunc(t, h0, h1)
-// 	})
+		testFunc(t, h0, h1)
+	})
 
-// 	t.Run("AsymmetricListen", func(t *testing.T) {
-// 		t.Parallel()
+	t.Run("AsymmetricListen", func(t *testing.T) {
+		t.Parallel()
 
-// 		tpt := inproc.New(inproc.WithEnv(inproc.NewEnv()))
+		tpt := inproc.New(inproc.WithEnv(inproc.NewEnv()))
 
-// 		h0, err := libp2p.New(context.Background(),
-// 			libp2p.Transport(tpt),
-// 			libp2p.ListenAddrStrings("/inproc/h0"))
-// 		require.NoError(t, err)
+		h0, err := libp2p.New(context.Background(),
+			libp2p.Transport(tpt),
+			libp2p.ListenAddrStrings("/inproc/h0"))
+		require.NoError(t, err)
 
-// 		h1, err := libp2p.New(context.Background(),
-// 			libp2p.Transport(tpt),
-// 			libp2p.NoListenAddrs)
-// 		require.NoError(t, err)
+		h1, err := libp2p.New(context.Background(),
+			libp2p.Transport(tpt),
+			libp2p.NoListenAddrs)
+		require.NoError(t, err)
 
-// 		testFunc(t, h0, h1)
-// 	})
-// }
+		testFunc(t, h0, h1)
+	})
+}
 
-// func testFunc(t *testing.T, h0, h1 host.Host) {
-// 	defer func() {
-// 		require.NoError(t, h0.Close())
-// 		require.NoError(t, h1.Close())
-// 	}()
+func testFunc(t *testing.T, h0, h1 host.Host) {
+	defer func() {
+		require.NoError(t, h0.Close())
+		require.NoError(t, h1.Close())
+	}()
 
-// 	h0.SetStreamHandler("/test", func(s network.Stream) {
-// 		defer func() { require.NoError(t, s.Close()) }()
-// 		io.Copy(s, bytes.NewBufferString("hello, world!"))
-// 	})
+	h0.SetStreamHandler("/test", func(s network.Stream) {
+		defer func() { require.NoError(t, s.Close()) }()
+		io.Copy(s, bytes.NewBufferString("hello, world!"))
+	})
 
-// 	err := h1.Connect(context.Background(), *host.InfoFromHost(h0))
-// 	require.NoError(t, err)
+	err := h1.Connect(context.Background(), *host.InfoFromHost(h0))
+	require.NoError(t, err)
 
-// 	s, err := h1.NewStream(context.Background(), h0.ID(), "/test")
-// 	require.NoError(t, err)
-// 	defer s.Close()
+	s, err := h1.NewStream(context.Background(), h0.ID(), "/test")
+	require.NoError(t, err)
+	defer s.Close()
 
-// 	var buf bytes.Buffer
-// 	io.Copy(&buf, s)
-// 	require.Equal(t, "hello, world!", buf.String())
-// }
+	var buf bytes.Buffer
+	io.Copy(&buf, s)
+	require.Equal(t, "hello, world!", buf.String())
+}
